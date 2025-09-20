82120098ddcfe224886b0944ebddb40f7729c527
R3Z3N / Saildot4K <oajapplefan@gmail.com>
2025-09-19 23:04:02 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -117,7 +117,7 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef HDD
-    else if (LoadHDDIRX() < 0) // only load HDD crap if filexio and iomanx are up and running
+    if (LoadHDDIRX() < 0) // only load HDD crap if filexio and iomanx are up and running
     {
         scr_setbgcolor(0x0000ff);
         scr_clear();
