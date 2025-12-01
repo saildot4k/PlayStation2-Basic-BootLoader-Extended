39d3ecc433905280dac39ac29a91509633effd68
saildot4k <oajapplefan@gmail.com>
2025-11-30 20:57:13 -0800

    Update banner.h

--- include/banner.h
+++ include/banner.h
@@ -5,7 +5,8 @@
 const char *BANNER =
 // change the banner text depending on system type, leave versioning and credits the same
     "PS2 Basic BootLoader - By Matias Israelson AKA: EL_isra \n"
-    "v" VERSION "-" SUBVERSION "-" PATCHLEVEL " - " STATUS \n"
+    "v" VERSION "-" SUBVERSION "-" PATCHLEVEL " - " STATUS
+    "\n"
     "https://ps2store.com - Modified - 9 Paths               \n"
 #ifdef PSX
     "\t\t__________  _____________  _______________________.____\n"
@@ -17,7 +18,7 @@ const char *BANNER =
 #else
     "\t       L1: PSBBN-FORWARDER               R1: NHDDL                \n"
     "\t       L2: OSDSYS                        R2: OPL                  \n"
-    "\n"
+    "\t                                                                  \n"
     "\t       /\\: OSD-XMB                      /\\:WLE-ISR              \n"
     "\t<: USER    >: RETROLauncher  []: POPSLoader  O: WLE-ISR (REVERSE) \n"
     "\t       \\/: XEB+                         X: DKWDRV                \n"
@@ -29,6 +30,6 @@ const char *BANNER =
 #endif
     "\n"
     "\n";
-#define BANNER_FOOTER \
+#define BANNER_FOOTER                                                   \
 
 #endif
