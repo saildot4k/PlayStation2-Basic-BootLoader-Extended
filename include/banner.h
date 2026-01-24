bec27731bfe19006584a194df83e79678010175c
saildot4k <oajapplefan@gmail.com>
2026-01-23 20:28:15 -0800

    Update backup config file and banners

--- include/banner.h
+++ include/banner.h
@@ -6,16 +6,17 @@ const char *BANNER =
 // change the banner text depending on system type, leave versioning and credits the same
     "\tPS2 Basic BootLoader - By Matias Israelson AKA: EL_isra \n"
     "\tv" VERSION "-" SUBVERSION "-" PATCHLEVEL " - " STATUS
-    "\n"
-    "\thttps://ps2store.com - Modified - 9 Paths               \n"
+    "\thttps://ps2homebrewstore.com - Modified - 9 Paths               \n"
     "\n"
 #ifdef PSX
-    "\t\t__________  _____________  _______________________.____\n"
-    "\t\t\\______   \\/   _____/\\   \\/  /\\______   \\______   \\    |\n"
-    "\t\t |     ___/\\_____  \\  \\     /  |    |  _/|    |  _/    |\n"
-    "\t\t |    |    /        \\ /     \\  |    |   \\|    |   \\    |___\n"
-    "\t\t |____|   /_______  //___/\\  \\ |______  /|______  /_______ \\\n"
-    "\t\t                  \\/       \\_/        \\/        \\/        \\/\n"
+    "\t       L1: PSBBN-FORWARDER               R1: NHDDL                \n"
+    "\t       L2: RESCUE.ELF                    R2: OPL                  \n"
+    "\t                                                                  \n"
+    "\t       /\\: OSD-XMB                      /\\:WLE-ISR              \n"
+    "\t<: USER    >: RETROLauncher  []: POPSLoader  O: WLE-ISR (REVERSE) \n"
+    "\t       \\/: XEB+                         X: DKWDRV                \n"
+    "\t                                                                  \n"
+    "\t     SELECT: DISC SKIP LOGO            START: DISC                \n"
 #else
     "\t       L1: PSBBN-FORWARDER               R1: NHDDL                \n"
     "\t       L2: OSDSYS                        R2: OPL                  \n"
@@ -23,7 +24,8 @@ const char *BANNER =
     "\t       /\\: OSD-XMB                      /\\:WLE-ISR              \n"
     "\t<: USER    >: RETROLauncher  []: POPSLoader  O: WLE-ISR (REVERSE) \n"
     "\t       \\/: XEB+                         X: DKWDRV                \n"
-    "\t     SELECT: DISC SKIP LOGO            START: DISC               \n"
+    "\t                                                                  \n"
+    "\t     SELECT: DISC SKIP LOGO            START: DISC                \n"
 #endif
     
 #ifdef DEBUG
