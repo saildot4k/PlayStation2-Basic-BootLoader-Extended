66d47ac89ee324c9b71036a2c869ee5bc21d96d1
saildot4k <oajapplefan@gmail.com>
2026-01-27 23:54:36 -0800

    Update banner_hotkeys.h

--- include/banner_hotkeys.h
+++ include/banner_hotkeys.h
@@ -26,8 +26,7 @@ const char *BANNER_HOTKEYS =
 #else
     "  PS2 Basic BootLoader v" VERSION "-" SUBVERSION "-" PATCHLEVEL " - " STATUS "- By Matias Israelson AKA: EL_isra \n"
 #endif
-    "  https://ps2homebrewstore.com - Modified - 10 Path, ARGV, Hotkey Display  \n"
-    "\n";
+    "\n\n";
 const char *BANNER_HOTKEYS_NAMES =
 #ifdef PSX
     "  __________  ____________  ___ \n"
