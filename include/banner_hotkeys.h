d376084950b91791d8c0611eab339c54ef77bca7
saildot4k <oajapplefan@gmail.com>
2026-01-27 23:59:56 -0800

    Update banner_hotkeys.h

--- include/banner_hotkeys.h
+++ include/banner_hotkeys.h
@@ -39,8 +39,8 @@ const char *BANNER_HOTKEYS_NAMES =
     "  _________  _________________  \n"
     "  \\______  \\/   _____/\\_____  \\  AUTO:     " NAME_AUTO "\n"
     "  |     ___/\\_____  \\  /  ____/  TRIANGLE: " NAME_TRIANGLE "\n"
-    "  |    |    /        \\ /      \\  CIRCLE:   " NAME_CIRCLE "\n"
-    "  |____|   /_______   /\\______ \\ CROSS:    " NAME_CROSS "\n"
+    "  |    |    /        \\/       \\  CIRCLE:   " NAME_CIRCLE "\n"
+    "  |____|   /_______  /\\_______ \\ CROSS:    " NAME_CROSS "\n"
     "                   \\/         \\/ SQUARE:   " NAME_SQUARE "\n"
 #endif
     " ____________________._____      UP:       " NAME_UP "\n"
