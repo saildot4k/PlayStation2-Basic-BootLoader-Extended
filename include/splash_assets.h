b46311bcd02d6d70f2bfe68f2f45ba8ccc06d1f6
saildot4k <oajapplefan@gmail.com>
2026-03-04 19:49:00 -0800

    Revert "use 8bit palleted clut instead"
    This reverts commit 91de17fd132292a9a9e978ab07d123290f7c195d.

--- include/splash_assets.h
+++ include/splash_assets.h
@@ -1,20 +1,11 @@
 #ifndef SPLASH_ASSETS_H
 #define SPLASH_ASSETS_H
 
-typedef enum
-{
-    SPLASH_PIXFMT_RBGA32 = 0,
-    SPLASH_PIXFMT_IDX8_CLUT32 = 1,
-} SPLASH_PIXFMT;
-
 typedef struct
 {
-    const unsigned char *pixels;
-    const unsigned char *clut_rgba;
-    unsigned int clut_entries;
+    const unsigned char *pixels_rbga;
     unsigned int width;
     unsigned int height;
-    SPLASH_PIXFMT format;
 } SPLASH_IMAGE;
 
 const SPLASH_IMAGE *SplashGetLogoImage(int is_psx_desr);
